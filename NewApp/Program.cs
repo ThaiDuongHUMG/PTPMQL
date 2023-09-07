10211956443fa0ecd6373243d0e70f3262cfd412
ThaiDuongHUMG <2021050162@student.humg.edu.vn>
2023-09-07 18:46:05 +0700

    btth

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+{
+// khai bao ho va ten
+const string hoTen="Nguyen Thai Duong";
+const int tuoi = 21;
+Console.WriteLine("{0} , {1}" , hoTen, tuoi);
+Console.WriteLine("Dia chi:{0} - MSV:{1}",diachi,Msv);
+}
+
