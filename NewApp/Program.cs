5fba65ec1b234f205bd6321560455ae0e8061ebe
ThaiDuongHUMG <2021050162@student.humg.edu.vn>
2023-09-28 15:58:19 +0700

    BTH

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,10 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.CompilerServices;
-{
-// khai bao ho va ten
-const string hoTen="Nguyen Thai Duong";
-const int tuoi = 21;
-Console.WriteLine("{0} , {1}" , hoTen, tuoi);
-Console.WriteLine("Dia chi:{0} - MSV:{1}",diachi,Msv);
-}
+﻿using Newapp.Models;
 
+Employee Emp = new Employee();
+Emp.Nhapthongtin();
+Emp.Hienthi();
